d6e3250a79fccf3bfe84e19f8b77989169e5cdd3
Keanen Lovgren <kboeholtlovgren@HS-15226.esd113.k12.wa.us>
2026-01-28 16:19:44 -0800

    Add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Purpose
+A Restic wrapper script written in pure POSIX shell.
+
+# Goals
+
+## Minimum Viable Product
+Run restic backup, check, forget, and prune according to a policy file.
+
+## Long Term
+- Notifications via apprise
+- Integrate with ZFS snapshots
+- Run arbitrary pre and post hook commands
+
+## Non-Goals
+- Windows/Powershell support
--- backup.sh
+++ backup.sh
@@ -2,20 +2,19 @@
 
 while getopts "Z:NcL:d:" flag; do
     case "$flag" in
-        Z) echo "Z $OPTARG";;
-        N) echo "Nice enabled :)"; nice=19;;
-        c) compression=max;;
-        L) bw_limit=$OPTARG;;
-        d) conf_dirs="$OPTARG\n$conf_dirs";;
-        *) echo "Invalid flag: $OPTARG"; exit 1
+    Z) echo "Z $OPTARG" ;;
+    N)
+        echo "Nice enabled :)"
+        NICE_VALUE=19
+        ;;
+    c) compression=max ;;
+    L) bw_limit=$OPTARG ;;
+    d) conf_dirs="$OPTARG\n$conf_dirs" ;;
+    *)
+        echo "Invalid flag: $OPTARG"
+        exit 1
+        ;;
     esac
-done
-
-
-echo "$conf_dirs"
-# Load our config file
-for conf in "$PWD"/*.conf.sh; do
-    echo "Found: $conf"
 done
 
 ################################################################################
@@ -27,8 +26,8 @@ backup() (
 
     cd "$BACKUP_DIR" || exit 1
 
-    nice -n "${nice:-0}" \
-    restic backup \
+    nice -n "${NICE_VALUE:-0}" \
+        restic backup \
         --no-scan=true \
         --compression="${compression:-auto}" \
         --limit-download="${bw_limit:-0}" \
@@ -38,14 +37,44 @@ backup() (
     backup_exit_code=$?
 
     case $backup_exit_code in
-        0) echo "SUCCESS: Backup created successfully"  ;;
-        1) echo "ERROR: Fatal error during backup";     exit 1;;
-        3) echo "ERROR: Inconsistent snapshot created"; exit 3;;
-        10) echo "ERROR: Repository does not exist";    exit 10;;
-        11) echo "ERROR: Repository is locked";         exit 11;;
-        12) echo "ERROR: Incorrect password";           exit 12;;
-        126) echo "ERROR: Could not execute restic";    exit 126;;
-        127) echo "ERROR: restic not found";            exit 127;;
-        *) echo "ERROR: Unknown error type";            exit 1
+    0) echo "SUCCESS: Backup created successfully" ;;
+    1)
+        echo "ERROR: Fatal error during backup"
+        exit 1
+        ;;
+    3)
+        echo "ERROR: Inconsistent snapshot created"
+        exit 3
+        ;;
+    10)
+        echo "ERROR: Repository does not exist"
+        exit 10
+        ;;
+    11)
+        echo "ERROR: Repository is locked"
+        exit 11
+        ;;
+    12)
+        echo "ERROR: Incorrect password"
+        exit 12
+        ;;
+    126)
+        echo "ERROR: Could not execute restic"
+        exit 126
+        ;;
+    127)
+        echo "ERROR: restic not found"
+        exit 127
+        ;;
+    *)
+        echo "ERROR: Unknown error type"
+        exit 1
+        ;;
     esac
+)
+
+check() (
+    nice -n "${NICE_VALUE:-0}" \
+        restic check
+
 )
